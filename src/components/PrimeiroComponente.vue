62e8c8ef71a24d17bb8e22d1bca2ea62ad37577f
caiorengp <caioribeiro.engenhariap@hotmail.com>
2025-05-15 19:58:02 -0300

    vue

---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+    <h1>Olá vue</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:"PrimeiroComponente",
+    data()
+ {
+    return {
+        nome:'Caio',
+        profissao: 'Programador',
+    }
+ }
+}
+</script>
